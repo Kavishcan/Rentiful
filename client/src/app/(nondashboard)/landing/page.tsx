7ab428320a7123adbf0449809f720bf47e51c570
Kavishcan <kavishcan2002@gmail.com>
2025-04-03 15:58:57 +0530

    add FeaturesSection component to landing page

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
 
 function Landing() {
   return (
     <div>
       <HeroSection />
+      <FeaturesSection />
     </div>
   );
 }
