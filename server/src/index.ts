367e44b9332956b1122469c6074274f3c7a766c1
Kavishcan <kavishcan2002@gmail.com>
2025-04-05 03:54:45 +0530

    update ESLint configuration; disable specific TypeScript rules and improve server route comments

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,14 +5,14 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import { authMiddleware } from "./middleware/authMiddleware";
-// ROUTE IMPORTS
+/* ROUTE IMPORT */
 import tenantRoutes from "./routes/tenantRoutes";
 import managerRoutes from "./routes/managerRoutes";
 import propertyRoutes from "./routes/propertyRoutes";
 import leaseRoutes from "./routes/leaseRoutes";
 import applicationRoutes from "./routes/applicationRoutes";
 
-// CONFIGURATIONS
+/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -23,19 +23,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-// ROUTES
+/* ROUTES */
 app.get("/", (req, res) => {
-  res.send("This is the home route");
+  res.send("This is home route");
 });
 
 app.use("/applications", applicationRoutes);
-app.use("/leases", leaseRoutes);
 app.use("/properties", propertyRoutes);
+app.use("/leases", leaseRoutes);
 app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
-// SERVER
-const port = process.env.PORT || 3002;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+/* SERVER */
+const port = Number(process.env.PORT) || 3002;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
